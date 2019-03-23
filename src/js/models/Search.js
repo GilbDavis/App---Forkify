5846f7fd3e8bbd94787f202d1a831805e5d4813e
Gilberto <peter01@hotmail.es>
2019-03-23 15:01:16 -0500

    Primer commit

---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+require("babel-polyfill");
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '2afd94af1f653f2e9c345a8c97a4a3ca';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
